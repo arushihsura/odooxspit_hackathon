c87bde2ad2b2b4bb9c3e61c18b755e8ff8e7faf5
arushihsura <arushi.coding7@gmail.com>
2025-11-22 09:34:55 +0530

    datbase connected

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,47 +1,37 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import pool from "../db/pool.js";
+import User from "../models/User.js";
 
-export const register = async (req, res) => {
+export const registerUser = async (req, res) => {
   const { name, email, password } = req.body;
 
-  const userExists = await pool.query(
-    "SELECT * FROM users WHERE email=$1",
-    [email]
-  );
-
-  if (userExists.rows.length > 0)
-    return res.json({ error: "User already exists" });
+  const exist = await User.findOne({ email });
+  if (exist) return res.json({ error: "User already exists" });
 
   const hashed = await bcrypt.hash(password, 10);
 
-  await pool.query(
-    "INSERT INTO users(name, email, password) VALUES($1,$2,$3)",
-    [name, email, hashed]
-  );
+  const user = new User({
+    name,
+    email,
+    password: hashed,
+  });
 
+  await user.save();
   res.json({ message: "User registered" });
 };
 
-export const login = async (req, res) => {
+
+export const loginUser = async (req, res) => {
   const { email, password } = req.body;
 
-  const data = await pool.query(
-    "SELECT * FROM users WHERE email=$1",
-    [email]
-  );
-
-  if (data.rows.length === 0)
-    return res.json({ error: "User not found" });
-
-  const user = data.rows[0];
+  const user = await User.findOne({ email });
+  if (!user) return res.json({ error: "User not found" });
 
   const isMatch = await bcrypt.compare(password, user.password);
-  if (!isMatch)
-    return res.json({ error: "Invalid password" });
+  if (!isMatch) return res.json({ error: "Invalid password" });
 
   const token = jwt.sign(
-    { id: user.id, email: user.email },
+    { id: user._id, email },
     process.env.JWT_SECRET,
     { expiresIn: "1h" }
   );
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,18 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
-const app = express();
+connectDB();
 
+const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Routes
 app.use("/auth", authRoutes);
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+app.listen(process.env.PORT, () =>
+  console.log(`Server running on port ${process.env.PORT}`)
+);
